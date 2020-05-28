27fcc91b2d2d54fdaf80cc2155df65ba96454f20
maryredlinger <mary@redlinger.net>
2020-05-28 15:11:17 -0700

    subtractions division addition and refresh addes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   numOne: string;
   numTwo: string;
   total = null
@@ -15,6 +14,19 @@ export class AppComponent {
 
   multiplyNums () {
       this.total = this.numOne * this.numTwo
+  };
+  addNums () {
+      let one = +this.numOne
+      let two = +this.numTwo
+
+      this.total = one + two
+  };
+  subtractNums () {
+      this.total = this.numOne - this.numTwo
+  };
+
+  divideNums () {
+      this.total = this.numOne / this.numTwo
   };
 
 }
